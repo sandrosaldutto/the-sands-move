2eab95b67a999bc0bbd34e8b59d615998149a8b7
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-08-02 14:13:55 -0400

    added fixed nav, new categories, outlined template.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,5 @@
 import "./Nav.scss";
-import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
 
 function Nav() {
   return (
@@ -8,20 +8,18 @@ function Nav() {
         <h1 className="nav__logo">Logo</h1>
         <div className="nav__container">
           <div className="nav__row">
-            <Link>Bedroom</Link>
-            <Link>Office</Link>
+          <Link className="nav__link">Bedroom</Link>
+          <Link className="nav__link">Office</Link>
+          <Link className="nav__link">Kitchen</Link>
           </div>
           <div className="nav__row">
-            <Link>Dining</Link>
-            <Link>Living</Link>
+          <Link className="nav__link">Dining</Link>
+          <Link className="nav__link">Living</Link>
+          <Link className="nav__link">Storage</Link>
           </div>
           <div className="nav__row">
-            <Link>Storage</Link>
-            <Link>Kitchen</Link>
           </div>
-          <div className="nav__row nav__row--single">
-            <Link>Misc</Link>
-          </div>
+          <Link className="nav__link">Misc</Link>
         </div>
       </section>
     </>
