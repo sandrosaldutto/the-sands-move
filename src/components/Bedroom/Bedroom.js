e466ebe724a192eb52d56d3428bd44bde63175ac
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-08-10 11:31:52 -0400

    experimented with react bootstrap to add the carousels and successfully made one

--- src/components/Bedroom/Bedroom.js
+++ src/components/Bedroom/Bedroom.js
@@ -1,17 +1,18 @@
 import "./Bedroom.scss";
+import bedPhotoOne from "../../assets/images/bedroom-bed-1.jpg";
+import bedPhotoTwo from "../../assets/images/bedroom-bed-2.jpg";
+import bedPhotoThree from "../../assets/images/bedroom-bed-3.jpg";
+import Carousel from "../Slideshow/Slideshow";
 
 function Bedroom() {
   return (
     <section className="bedroom">
       <h1 className="bedroom__title">Bedroom</h1>
-    <div className="bedroom__gallery">
-      <div className="bedroom__card"></div>
-      <div className="bedroom__card"></div>
-      <div className="bedroom__card"></div>
-      <div className="bedroom__card"></div>
-      <div className="bedroom__card"></div>
-      <div className="bedroom__card"></div>
-    </div>
+      <div className="bedroom__gallery">
+        <div className="bedroom__card">
+          <Carousel/>
+        </div>
+      </div>
     </section>
   );
 }
---
+++ src/components/Slideshow/Slideshow.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Carousel } from "react-bootstrap";
+import bedPhotoOne from "../../assets/images/bedroom-bed-1.jpg";
+import bedPhotoTwo from "../../assets/images/bedroom-bed-2.jpg";
+import bedPhotoThree from "../../assets/images/bedroom-bed-3.jpg";
+
+function Slideshow() {
+    return (
+        <>
+        <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={bedPhotoOne}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>
+              Nulla vitae elit libero, a pharetra augue mollis interdum.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={bedPhotoTwo}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={bedPhotoThree}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl
+              consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      </>
+    );
+  }
+  
+  export default Slideshow;
